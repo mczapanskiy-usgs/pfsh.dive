deb87ec56a6044f9a869a2b73b9e4d257bdc1b1e
Max Czapanskiy <maxczapanskiy@gmail.com>
2018-02-06 16:59:02 -0800

    fix ggplot bug

--- R/plot_dive.R
+++ R/plot_dive.R
@@ -51,23 +51,24 @@ plot_dive <- function(calib_tdr, diveid, surface_thr = .1) {
     pmin(c(0, Inf)) %>%
     rev
   expand_sec <- 2
+  break_fun <- function(lim) seq(lim[1], lim[2], by = 0.5)
   dive_plot <- event %>%
     filter(between(UTC, dive_begin - expand_sec, dive_end + expand_sec)) %>%
     mutate(InDive = factor(!is.na(DiveID) & DiveID == diveid)) %>%
-    ggplot(aes(x = UTC, y = CalibPressure)) +
-    geom_line(color = 'blue') +
-    geom_point(aes(color = InDive)) +
-    geom_hline(yintercept = surface_thr,
-               color = 'blue',
-               linetype = 'dashed') +
-    ylim(dive_press_lim) +
-    scale_x_datetime(date_labels = '%H:%M:%OS1',
-                     breaks = function(lim) seq(lim[1], lim[2], by = 0.5)) +
-    scale_color_manual(values = c('red', 'blue')) +
-    theme(legend.position = 'none') +
-    labs(x = 'Time (UTC)',
-         y = 'Depth (m)',
-         title = sprintf('DeployID %s DiveID %s', deployid, diveid))
+    ggplot2::ggplot(aes(x = UTC, y = CalibPressure)) +
+    ggplot2::geom_line(color = 'blue') +
+    ggplot2::geom_point(aes(color = InDive)) +
+    ggplot2::geom_hline(yintercept = surface_thr,
+                        color = 'blue',
+                        linetype = 'dashed') +
+    ggplot2::ylim(dive_press_lim) +
+    ggplot2::scale_x_datetime(date_labels = '%H:%M:%OS1',
+                              breaks = break_fun) +
+    ggplot2::scale_color_manual(values = c('red', 'blue')) +
+    ggplot2::theme(legend.position = 'none') +
+    ggplot2::labs(x = 'Time (UTC)',
+                  y = 'Depth (m)',
+                  title = sprintf('DeployID %s DiveID %s', deployid, diveid))
 
   # Event plot
   event_press_lim <- range(dive$Pressure,
@@ -77,27 +78,27 @@ plot_dive <- function(calib_tdr, diveid, surface_thr = .1) {
     rev
   event_plot <- event %>%
     mutate(InDive = factor(!is.na(DiveID) & DiveID == diveid)) %>%
-    ggplot(aes(x = UTC)) +
-    geom_line(aes(y = Pressure), color = 'red') +
-    geom_point(aes(y = Pressure), color = 'red') +
-    geom_line(aes(y = CalibPressure), color = 'green') +
-    geom_point(aes(y = CalibPressure), color = 'green') +
-    geom_hline(yintercept = surface_thr,
-               color = 'blue',
-               linetype = 'dashed') +
-    annotate('rect',
-             xmin = dive_begin,
-             xmax = dive_end,
-             ymin = event_press_lim[2],
-             ymax = event_press_lim[1],
-             alpha = 0.2,
-             fill = 'black') +
-    ylim(event_press_lim) +
-    scale_x_datetime(date_labels = '%H:%M:%OS1',
-                     breaks = function(lim) seq(lim[1], lim[2], by = 0.5)) +
-    labs(x = 'Time (UTC)',
-         y = 'Depth (m)',
-         title = '')
+    ggplot2::ggplot(aes(x = UTC)) +
+    ggplot2::geom_line(aes(y = Pressure), color = 'red') +
+    ggplot2::geom_point(aes(y = Pressure), color = 'red') +
+    ggplot2::geom_line(aes(y = CalibPressure), color = 'green') +
+    ggplot2::geom_point(aes(y = CalibPressure), color = 'green') +
+    ggplot2::geom_hline(yintercept = surface_thr,
+                        color = 'blue',
+                        linetype = 'dashed') +
+    ggplot2::annotate('rect',
+                      xmin = dive_begin,
+                      xmax = dive_end,
+                      ymin = event_press_lim[2],
+                      ymax = event_press_lim[1],
+                      alpha = 0.2,
+                      fill = 'black') +
+    ggplot2::ylim(event_press_lim) +
+    ggplot2::scale_x_datetime(date_labels = '%H:%M:%OS1',
+                              breaks = break_fun) +
+    ggplot2::labs(x = 'Time (UTC)',
+                  y = 'Depth (m)',
+                  title = '')
 
   # Dive and event plots side-by-side
   gridExtra::grid.arrange(dive_plot, event_plot, ncol = 2, widths = c(3, 2))
